662098c677b6ebd10ed018a307cffeb9361a9105
Alex Lozano <19170025@uttcampus.edu.mx>
2021-01-19 23:32:50 -0600

    Form cambiado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsComponent } from './forms/forms.component';
 import { AnimationComponent } from './animation/animation.component';
 import { Page404Component } from './page404/page404.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { OpenCloseComponent } from './open-close/open-close.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormsComponent,
     AnimationComponent,
     Page404Component,
     HomeComponent,
-    OpenCloseComponent
+    OpenCloseComponent,
+    NameEditorComponent,
+    ProfileEditorComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+export type EditorType = 'name' | 'profile';
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
@@ -10,6 +10,19 @@ export class FormsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  editor: EditorType = 'name';
+
+  get showNameEditor() {
+    return this.editor === 'name';
+  }
+
+  get showProfileEditor() {
+    return this.editor === 'profile';
+  }
+
+  toggleEditor(type: EditorType) {
+    this.editor = type;
   }
 
 }
