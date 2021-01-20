7260160be43b21a4ea84c4b24db0774af72e5efd
Alex Lozano <19170025@uttcampus.edu.mx>
2021-01-19 22:52:34 -0600

    Cambios hechos en el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnimationComponent } from './animation/animation.component';
+import { FormsComponent } from './forms/forms.component';
+import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'animation', component: AnimationComponent},
+  {path: 'form', component:FormsComponent},
+  {path: '**', component:Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
